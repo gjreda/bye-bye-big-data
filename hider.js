8a9c839c430446376dfa210831a6481f3794f122
Greg Reda <gjreda+github@gmail.com>
2014-05-11 18:56:28 -0500

    Remove counter that I never actually implemented

--- hider.js
+++ hider.js
@@ -1,7 +1,6 @@
 // Icon created by: http://icons8.com/
 
 var re = new RegExp('big.data|bigdata'); // in case I want to add more terms later
-var i = 0;
 
 $('a').each(function() {
     if ( this.href.toLowerCase().match(re) || this.innerText.toLowerCase().match(re)) {
